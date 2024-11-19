50322a5407496c2e0ce1bf36a154630cfc8cc0da
V0odo0 <Voodoo2211@gmail.com>
2024-11-20 01:01:27 +0500

    0.1.1

--- Editor/Scripts/UI/KnotRcPatcherProjectSettingsEditor.cs
+++ Editor/Scripts/UI/KnotRcPatcherProjectSettingsEditor.cs
@@ -1,12 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using Knot.Core.Editor;
+using UnityEditor;
+using UnityEditor.Compilation;
 using UnityEngine;
 
 namespace Knot.RCPatcher.Editor
 {
-    internal class KnotRcPatcherProjectSettingsEditor : ProjectSettingsEditor<KnotRcPatcherProjectSettingsEditor>
+    [CustomEditor(typeof(KnotRcPatcherProjectSettings))]
+    internal class KnotRcPatcherProjectSettingsEditor : ProjectSettingsEditor<KnotRcPatcherProjectSettings>
     {
-        
+        internal static string SettingsPath = $"Project/KNOT/RC Patcher";
+
+        [SettingsProvider]
+        static SettingsProvider GetSettingsProvider()
+        {
+            return GetSettingsProvider(KnotRcPatcher.ProjectSettings, SettingsPath,
+                typeof(KnotRcPatcherProjectSettingsEditor));
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            return;
+
+            serializedObject.Update();
+
+            var customSettings = serializedObject.FindProperty("_customProfile");
+            EditorGUILayout.PropertyField(customSettings);
+            serializedObject.ApplyModifiedProperties();
+
+            EditorGUILayout.Space(10);
+
+            if (customSettings.objectReferenceValue == null)
+                base.OnInspectorGUI();
+        }
+
+        [MenuItem("Tools/Foo")]
+        public static void Foo()
+        {
+            var pa = CompilationPipeline.GetAssemblyDefinitionFilePathFromAssemblyName("Knot.RCPatcher.Editor");
+            Debug.Log(pa);
+        }
     }
 }
